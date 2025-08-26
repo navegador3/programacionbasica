eb0ff6641af5637bdb3795138dcce85b35ba278b
MiguelSalazar <elnavegador.389@gmail.com>
2025-08-26 10:20:35 -0500

    modificando script

---
+++ function.js
@@ -0,0 +1,6 @@
+ function cambiarcolor(){
+       // alert("HOLA BIEMVENIDO");
+        document.body.style.backgroundColor = "black";
+        document.getElementById("title").style.color = "orange";
+        
+    }
